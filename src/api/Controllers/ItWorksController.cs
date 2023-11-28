e4184fd8d07a0c6b097aa9341d5eb8c0f6572b98
DnTo <danto@hotmail.com>
2023-11-27 21:44:10 -0500

    chore(back): add api

---
+++ src/api/Controllers/ItWorksController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace s12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItWorksController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello, It works!!!";
+        }
+
+        [HttpPost]
+        public string Post(PostModel model)
+        {
+            return $"Hello {model.Name}, It works!!!";
+        }
+
+        public class PostModel
+        {
+            public string Name { get; set; }
+        }
+    }
+}
