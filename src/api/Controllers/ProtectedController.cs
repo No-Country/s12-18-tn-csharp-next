f6f59e06d1962cebd285a677f46c1f55b75d4b7a
DnTo <danto@hotmail.com>
2023-11-30 23:48:26 -0500

    feat(back): endpoint for auth testing
    if authenticated, will return all claims for that user

---
+++ src/api/Controllers/ProtectedController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace s12.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProtectedController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public ActionResult<object> Get()
+        {
+            return new {
+                Message = $"You are authenticated",
+                Claims = User.Claims.Select(x => new { Type = x.Type, Value = x.Value }).ToList()
+            };
+        }
+    }
+}
