cd53f66030f1e6783ce76010ef975b5cc72f3b94
GoRiJ <jolem824@gmail.com>
2023-11-30 19:44:51 -0600

    feat: modelo de las rutas de autenticacion en la api

--- client/src/app/(auth)/services/auth-api-service.ts
+++ client/src/app/(auth)/services/auth-api-service.ts
@@ -1,12 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+import { BASE_URL } from "@/models";
+import { BASE_API_AUTH_PATH } from "@/app/(auth)/models";
+
 /**
  * Servicio de la api de autenticación.
  */
 export const authApi = createApi({
     reducerPath: "auth",
     baseQuery: fetchBaseQuery({
-        baseUrl: `any`
+        baseUrl: `${BASE_URL}${BASE_API_AUTH_PATH}`
     }),
     endpoints: (_builder) => ({})
 });
