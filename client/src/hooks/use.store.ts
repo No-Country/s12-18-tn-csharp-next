c0033a4c0e38aaf7683c90959003f4c47a8ffe8a
GoRiJ <jolem824@gmail.com>
2023-12-02 20:00:58 -0600

    feat: store models added

--- client/src/hooks/use.store.ts
+++ client/src/hooks/use.store.ts
@@ -1,9 +1,10 @@
 import { useDispatch, useSelector, type TypedUseSelectorHook } from "react-redux";
+import type { RootState, AppDispatch } from "@/models";
 
 /**
  * Hook para devolver un estado global específico de a aplicación.
  */
-export const useAppSelector: TypedUseSelectorHook = useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 /**
  * Hook para despachar una acción específica de los estados globales de la aplicación.
