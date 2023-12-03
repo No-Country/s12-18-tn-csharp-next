fbf546e1685db4612264c14ce4f6f60b503256b0
Sebastián Perichón <97280705+modestsp@users.noreply.github.com>
2023-12-03 05:02:24 -0300

    build(back): changed .net framework from .net8 to .net7 (#27)
    * Changed .net8 to .net7 packages
    
    * Added migrations for Events  and Events_Complaints

---
+++ src/api/Migrations/20231202211243_Added Events and Events_Complaints.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace s12.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEventsandEvents_Complaints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
