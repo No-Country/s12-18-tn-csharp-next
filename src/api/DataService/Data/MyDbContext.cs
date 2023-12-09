530a3659803bd7c0ab8c7af6894439db1bbdba32
modestsp <csperichon@gmail.com>
2023-12-09 19:12:25 -0300

    Added Event and Complaint entities to context

--- src/api/DataService/Data/MyDbContext.cs
+++ src/api/DataService/Data/MyDbContext.cs
@@ -11,12 +11,33 @@ public class MyDbContext : IdentityDbContext<User>
     }
 
     public DbSet<Donation> Donations { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Complaint> Complaints { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
         builder
             .Entity<User>()
             .OwnsOne(u => u.Bank_Details, builder =>
+            {
+                builder.ToJson();
+            });
+
+        builder
+            .Entity<Event>()
+            .OwnsOne(e => e.Geo, builder =>
+            {
+                builder.ToJson();
+            })
+            .OwnsMany(e => e.Media_Collection, builder =>
+            {
+                builder.ToJson();
+            });
+
+
+        builder
+            .Entity<Complaint>()
+            .OwnsMany(e => e.Media_Collection, builder =>
             {
                 builder.ToJson();
             });
