c8b3ab7ef1fdd020bb2ec0258e123369f9704bf4
modestsp <csperichon@gmail.com>
2023-12-09 19:10:57 -0300

    Added Services for CRUD operations

--- src/api/Controllers/EventsController.cs
+++ src/api/Controllers/EventsController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using s12.Entities.DbSet;
+using s12.Entities.Dtos.Requests;
+using s12.Entities.Dtos.Responses;
 using s12.Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -15,46 +18,55 @@ namespace s12.Controllers
         private Events_Service _events_Service;
         public EventsController(Events_Service events_Service) => _events_Service = events_Service;
 
-        // GET: <EventsController>
-        [HttpGet] public IEnumerable<Event_Get> Get(int pageSize = 0, int pageNumber = 0) => _events_Service.Events;
+
+        [HttpGet]
+        async public Task<ActionResult<IEnumerable<Event>>> Get_Events(int pageSize = 10, int pageNumber = 0)
+        {
+            var result = await _events_Service.GetEvents(pageSize, pageNumber);
+            if (!result.isSuccessfully)
+                return BadRequest(result.message);
+
+            return Ok(result.Events);
+        }
 
         [HttpGet("{event_Id}")]
-        public ActionResult<IEnumerable<Event_Get>> Get(int event_Id)
+        public async Task<ActionResult<Get_Event_Response>> Get_Event_By_Id(int event_Id)
         {
-            var e = _events_Service.Events.FirstOrDefault(x => x.Event_Id == event_Id);
-            return e is null ? NotFound() : Ok(e);
+            var result = await _events_Service.GetEvent(event_Id);
+            if (!result.isSuccessfully)
+                return NotFound();
+            return Ok(result.Event);
         }
 
         // POST: Event
         [HttpPost]
-        //[Authorize]
-        public ActionResult<IEnumerable<string>> Post(Event_Post new_Event)
+        [Authorize(Policy = "StandardRights")]
+        public async Task<ActionResult> Create_Event([FromForm] Create_Event_Request request)
         {
-            var e = new Event_Get();
+            var user_Email = User.FindFirst("Email")?.Value;
+            var user_Id = User.FindFirst("Id")?.Value;
+            var result = await _events_Service.CreateEvent(request, user_Id, user_Email);
 
-            e.Created_Date = DateOnly.FromDateTime(DateTime.Now);
-            e.Created_By_User = "GetTheUserFromSession";
-            e.Event_Id = _events_Service.Events.OrderBy(x => x.Event_Id).Last().Event_Id + 1;
-            e.Geo = new_Event.Geo;
+            if (!result.isSuccessfully)
+                return BadRequest(result.message);
 
-            _events_Service.Events.Add(e);
+            return CreatedAtAction(nameof(Get_Event_By_Id), new { event_Id = result.event_Created.Id }, result.event_Created);
 
-            return CreatedAtAction(nameof(Get), new { event_Id = e.Event_Id }, e);
         }
 
-        [HttpPut("{event_Id}/Media")]
-        //[Authorize]
-        public ActionResult<IEnumerable<Media[]>> Post_Media(int event_Id, [FromForm] IFormFile[] media)
-        {
-            var e = _events_Service.Events.FirstOrDefault(x => x.Event_Id == event_Id);
-            if (e is null) return BadRequest();
+        // [HttpPut("{event_Id}/Media")]
+        // //[Authorize]
+        // public ActionResult<IEnumerable<Media[]>> Post_Media(int event_Id, [FromForm] IFormFile[] media)
+        // {
+        //     var e = _events_Service.Events.FirstOrDefault(x => x.Event_Id == event_Id);
+        //     if (e is null) return BadRequest();
 
-            var _media = media.Select(x => new Media() { Type = x.ContentType, Url = $"somePlaceOnserver/{x.FileName}" });
-            if (e.Media is null) e.Media = new Media[0];
-            e.Media = e.Media.Concat(_media).ToArray();
+        //     var _media = media.Select(x => new Media() { Type = x.ContentType, Url = $"somePlaceOnserver/{x.FileName}" });
+        //     if (e.Media is null) e.Media = new Media[0];
+        //     e.Media = e.Media.Concat(_media).ToArray();
 
-            return CreatedAtAction(nameof(Get), new { event_Id = e.Event_Id }, e.Media);
-        }
+        //     return CreatedAtAction(nameof(Get), new { event_Id = e.Event_Id }, e.Media);
+        // }
     }
 
     //TODO move this classes to common
@@ -69,7 +81,7 @@ namespace s12.Controllers
         public string Description { get; set; }
         public decimal Collect_Goal { get; set; }
         public decimal Collected { get; set; }
-        public int Donors_Count { get;set; }
+        public int Donors_Count { get; set; }
         public Media[] Media { get; set; }
         public Geo Geo { get; set; }
 
@@ -87,26 +99,7 @@ namespace s12.Controllers
         public Geo Geo { get; set; }
     }
 
-    public class Media
-    {
-        public string Type { get; set; }
-        public string Url { get; set; }
-    }
 
-    public enum Media_Type
-    {
-        Image,
-        Video,
-        Audio,
-        Document,
-    }
 
-    public class Geo
-    {
-        public string Country { get; set; }
-        public string Provice { get; set; }
-        public string City { get; set; }
-        public double Lat { get; set; }
-        public double Long { get; set; }
-    }
+
 }
--- src/api/Controllers/Events_ComplaintsController.cs
+++ src/api/Controllers/Events_ComplaintsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Storage;
+using s12.Entities.DbSet;
+using s12.Entities.Dtos.Requests;
 using s12.Services;
 using System.ComponentModel.DataAnnotations;
 
@@ -13,72 +15,36 @@ namespace s12.Controllers
     public partial class EventsController : ControllerBase
     {
         [HttpGet("{event_id}/complaints")]
-        public ActionResult<IEnumerable<Complaint>> Event_Complaints(int event_id, int pageSize = 0, int ageNumber = 0)
+        public async Task<ActionResult<IEnumerable<Complaint>>> Event_Complaints(int event_id, int pageSize = 0, int ageNumber = 0)
         {
-            //TODO this is part of service, here only for mocked data
-            var eve = _events_Service.Events.FirstOrDefault(x => x.Event_Id == event_id);
+            var result = await _events_Service.Get_Complaints_From_Event(event_id, pageSize, ageNumber);
+            if (!result.isSuccessfully)
+                return BadRequest(result.message);
 
-            if (eve is not null)
-            {
-                return Ok(eve.Complaints ?? Enumerable.Empty<Complaint>());
-            }
-            return BadRequest();
+            return Ok(result.Complaints);
         }
 
         [HttpGet("{event_id}/complaints/{complaint_Id}")]
-        public ActionResult<IEnumerable<Complaint>> Event_Complaints(int event_id, int complaint_Id)
+        public async Task<ActionResult<Complaint>> Get_Complaint_From_Event(int event_id, int complaint_Id)
         {
-            //TODO this is part of service, here only for mocked data
-            var eve = _events_Service.Events.FirstOrDefault(x => x.Event_Id == event_id);
+            var result = await _events_Service.Get_Single_Complaint_From_Event(event_id, complaint_Id);
 
-            if (eve is not null && eve.Has_Complaints)
-            {
-                var complaint = eve.Complaints.FirstOrDefault(x => x.Complaint_Id == complaint_Id);
-                return complaint is null ? NotFound() : Ok(complaint);
-            }
+            if (!result.isSuccessfully)
+                return BadRequest();
 
-            return NotFound();
+            return Ok(result.Complaint);
         }
 
         //[Authorize]
         [HttpPost("{event_id}/complaints")]
-        public ActionResult<IEnumerable<Complaint>> Post_Event_Complaints(int event_id, [FromForm]Complaint_Post new_Complaint)
+        public async Task<ActionResult<IEnumerable<Complaint>>> Create_Event_Complaint(int event_id, [FromForm] Create_Complaint_Request request)
         {
+            var result = await _events_Service.Create_Complaint(event_id, request);
 
-            //TODO All This to service
-            var e = _events_Service.Events.FirstOrDefault(x => x.Event_Id == event_id);
-            if (e is not null)
-            {
-                var c = new Complaint();
-                if (c.Media is null) c.Media = new List<Media>();
-                if (e.Complaints is null) e.Complaints = new List<Complaint>();
+            if (!result.isSuccessfully)
+                return BadRequest(result.message);
 
-                c.Complaint_Id = 1 + (e.Complaints.Any() ? e.Complaints.Select(x => x.Complaint_Id).OrderBy(x => x).LastOrDefault() : 0);
-                c.Complaint_Date = DateOnly.FromDateTime(DateTime.Now);
-                c.Reporter_Id = -1;
-                c.Reporter_Name = "FromSession";
-
-                c.Title = new_Complaint.Title;
-                c.Description = new_Complaint.Description;
-
-                e.Complaints.Add(c);
-
-                if (new_Complaint.Media is not null)
-                {
-                    foreach (var item in new_Complaint.Media)
-                    {
-                        c.Media.Add(new Media()
-                        {
-                            Type = item.ContentType,
-                            Url = $"somePlaceOnserver/{item.FileName}"
-                        });
-                    }
-                }
-
-                return CreatedAtAction(nameof(Event_Complaints), new { event_id = event_id, complaint_Id = c.Complaint_Id }, c);
-            }
-
-            return BadRequest();
+            return CreatedAtAction(nameof(Event_Complaints), new { event_id }, result.Complaint_Created);
         }
     }
 
@@ -93,14 +59,5 @@ namespace s12.Controllers
 
 
     //TODO move to common or entities
-    public class Complaint
-    {
-        public int Complaint_Id { get; set; }
-        public DateOnly Complaint_Date { get; set; }
-        public int Reporter_Id { get; set; }
-        public string Reporter_Name { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public IList<Media> Media { get; set; }
-    }
+
 }
